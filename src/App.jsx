e314de8e5b7c62d3ed389df1d6848f11f3c0a3b8
Sivak Hico <strukggg@gmail.com>
2022-10-06 17:39:41 +0200

    working version display the data json

--- src/App.jsx
+++ src/App.jsx
@@ -1,65 +1,21 @@
 import { useEffect, useState } from 'react'
 import BlogList from './BlogList'
 import './App.css'
-import PassengersList from './Passengers'
 
 export default function App() {
-  const [blogs, setBlogs] = useState([
-    { id: 1, name: 'Sevo', text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry' },
-    { id: 2, name: 'Moris', text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry' },
-    { id: 3, name: 'Sevo', text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry' }])
-
-  const [comment, setComment] = useState([])
-  const [search, setSearch] = useState('')
-  const [currentPage, setCurrentPage] = useState(1);
+  const [blogs, setBlogs] = useState(null)
 
   useEffect(() => {
-    fetch('https://hn.algolia.com/api/v1/search?query=car')
-      .then((res) => res.json())
-      .then((obj) => setComment(obj.hits))
+    fetch('http://localhost:3000/blogs')
+      .then(res => { return res.json() })
+      .then(data => setBlogs(data))
   }, [])
-
-  const searchFor = (e) => {
-    setSearch(e.target.value)
-  }
-
-  const deletBlog = (id) => {
-    const newBlogs = blogs.filter(blog => blog.id !== id)
-    setBlogs(newBlogs)
-  }
 
   return (
     <div className="App">
-      <BlogList blogs={blogs} title='All Blogs' deletBlog={deletBlog} />
-      <BlogList blogs={blogs.filter(blog => blog.name === 'Sevo')} title='Sevo Blogs' />
-      <hr />
-      <h1>Fetch Data From Api</h1>
-      <div>
-        <input onChange={searchFor} value={search} type="text" placeholder='Search for what ...' />
-        <span>{search}</span>
-      </div>
-
       <div className="card">
-        {comment.map((c) =>
-        (
-          <>
-            <div>
-              <li>TITLE: {c.title}</li>
-              <li>AUTHOR: {c.author}</li>
-              <li>POINTS: {c.points}</li>
-              <li>CREATED: {c.created_at_i}</li>
-              <li>TAGS: {c._tags.map((tag, i) =>
-                <ol start={i + 1}>
-                  <li>{tag}</li>
-                </ol>
-              )}</li>
-              <li><a href={c.url}>webSite</a></li>
-            </div>
-            <hr />
-          </>
-        ))}
+        {blogs && <BlogList blogs={blogs} title='All Blogs!' />}
       </div>
-      <PassengersList />
     </div>
   )
 }
--- src/BlogList.jsx
+++ src/BlogList.jsx
@@ -1,12 +1,11 @@
-export default function BlogList({ blogs, title, deletBlog }) {
+export default function BlogList({ blogs, title }) {
     return (
         <div>
             <h1>{title}</h1>
             {blogs.map((blog) => (
                 <div className='display-blog' key={blog.id}>
-                    <h3>{blog.name}</h3>
-                    <p>{blog.text}</p>
-                    <button onClick={() => deletBlog(blog.id)}>Remove</button>
+                    <h3>{blog.title}</h3>
+                    <p>{blog.author}</p>
                 </div>
             ))}
         </div>
